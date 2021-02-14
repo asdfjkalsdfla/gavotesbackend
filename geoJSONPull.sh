a9ce959254f053a845a33b3d5bc158e39b914886
Tom Thumb <github@asdfjkalsdfla.com>
2021-02-14 11:04:28 -0500

    pull 2020 shape file

--- geoJSONPull.sh
+++ geoJSONPull.sh
@@ -5,21 +5,27 @@ set -x
 GEOJSON_DATA_DIR="data/geojson/"
 
 declare -A shape0=(
+    [year]='2020'
+    [dlFile]='https://dataverse.harvard.edu/api/access/datafile/4172134?gbrecs=true'
+    [extractFile]="ga_2020_general.shp"
+)
+
+declare -A shape1=(
     [year]='2018'
     [dlFile]='https://www.legis.ga.gov/api/document/docs/default-source/reapportionment-document-library/vtd2018-shapefile.zip?sfvrsn=d01286c5_2'
     [extractFile]="VTD2018-Shapefile.shp"
 )
-declare -A shape1=(
+declare -A shape2=(
     [year]='2016'
     [dlFile]="https://www.legis.ga.gov/api/document/docs/default-source/reapportionment-document-library/vtd2016-shape.zip?sfvrsn=2a170a08_2"
     [extractFile]="VTD2016-Shape.shp"
 )
-declare -A shape2=(
+declare -A shape3=(
     [year]='2014'
     [dlFile]="https://www.legis.ga.gov/api/document/docs/default-source/reapportionment-document-library/vtd2014-shape.zip?sfvrsn=c3a31adc_2"
     [extractFile]="VTD2014-SHAPE.shp"
 )
-declare -A shape3=(
+declare -A shape4=(
     [year]='2012'
     [dlFile]="https://www.legis.ga.gov/api/document/docs/default-source/reapportionment-document-library/voting-precinct-2012.zip?sfvrsn=952212ae_2"
     [extractFile]="VTD12.shp"
@@ -32,7 +38,9 @@ for shape in ${!shape@}; do
     # echo $GEOJSON_YEAR
     mkdir -p $GEOJSON_DATA_DIR/$GEOJSON_YEAR
     DLFile="$GEOJSON_DATA_DIR/$GEOJSON_YEAR/`date +%Y%m%d`.zip"
-    curl "${shape[dlFile]}" -o $DLFile
+    curl "${shape[dlFile]}" -L -o $DLFile
     unzip -o $DLFile -d $GEOJSON_DATA_DIR/$GEOJSON_YEAR
     yarn shp2json data/geojson/$GEOJSON_YEAR/${shape[extractFile]} -o data/geojson/$GEOJSON_YEAR/GeoJSON$GEOJSON_YEAR.json
 done 
+
+
