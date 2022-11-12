da2190d04db31c25c90b2563bf714713812ab115
Tom Thumb <github@asdfjkalsdfla.com>
2022-11-11 20:58:27 -0500

    add manual overrides

--- electionResultPrecinctNamelToID.py
+++ electionResultPrecinctNamelToID.py
@@ -5,17 +5,22 @@ import argparse
 
 parser = argparse.ArgumentParser()
 parser.add_argument("year")
+parser.add_argument("--county", help="specifies the county")
 args = parser.parse_args()
 year = args.year
 
 electionResults = pd.read_csv('data/electionResults/'+year+'/'+year+'_general.csv')
 electionResultsCountiesAndPrecincts = electionResults[['county','precinct']].drop_duplicates()
 electionResultsCountiesAndPrecincts['county'] = electionResults['county'].str.upper()
+if args.county:
+    electionResultsCountiesAndPrecincts = electionResultsCountiesAndPrecincts[electionResultsCountiesAndPrecincts['county'] == args.county.upper()]
 electionResultsCounties = electionResultsCountiesAndPrecincts['county'].unique().tolist()
 
 
 mapCountyPrecinctList = pd.read_csv('data/geojson/precincts/2020_simple/GA_precincts_id_to_name.csv')
 mapCountyPrecinctList['matchLabel'] = mapCountyPrecinctList['precinctName']
+
+manualMapLabelToOverrides = pd.read_csv('data/electionResults/'+year+'/'+year+'_general_map_ids_manual.csv', dtype={'precinct':'string'})
 
 f = open('data/electionResults/'+year+'/'+year+'_general_map_ids.csv', "w")
 f.write('county,precinct,mapPrecinctName,electionResultsPrecinctName,score\n')
@@ -24,8 +29,19 @@ for county in electionResultsCounties :
     mapPrecincts = mapCountyPrecinctList[mapCountyPrecinctList['county']==county]
     mapPrecinctsLabels = mapPrecincts['matchLabel'].tolist()
     precinctsInCounty = electionResultsCountiesAndPrecincts[electionResultsCountiesAndPrecincts['county']==county]['precinct'].tolist()
+    # print(precinctsInCounty)
+    mapPrecinctsInCountyID = mapCountyPrecinctList[mapCountyPrecinctList['county']==county]['precinct'].tolist()
+    # print(mapPrecinctsInCountyID)
+    # print(mapPrecinctsLabels)
+    
     for precinct in precinctsInCounty :
-        if county == "CHATHAM" or county == "WARE" or county == "FORSYTH" :
+        overrides = manualMapLabelToOverrides[( (manualMapLabelToOverrides['county']==county) & (manualMapLabelToOverrides['electionResultsPrecinctName']==precinct))]
+        if(overrides.size > 0) : 
+            # precinctID = overrides.at[0, 'precinct']
+            precinctID = overrides.iat[0, 1]
+            label = overrides.iat[0, 2]
+            mapLabelMatched=(label,100)
+        elif county == "CHATHAM" or county == "WARE" or county == "FORSYTH" :
             if (year == "2018" or year == "2016"):
                 precinctID = precinct.split(" ")[0]+"C"
             else:
@@ -50,9 +66,8 @@ for county in electionResultsCounties :
             mapLabelMatched = process.extractOne(precinct, mapPrecinctsLabels)
             precinctID = mapPrecincts[mapPrecincts['matchLabel']==mapLabelMatched[0] ]['precinct'].tolist()[0]
         
-        line  = "{county},{precinctID},{mapLabelMatched},{precinct},{score:.2f}".format(county=county,precinctID=precinctID,precinct=precinct,mapLabelMatched=mapLabelMatched[0],score=mapLabelMatched[1])
-        if mapLabelMatched[1] > 50 or len(precinctsInCounty)==0 :
-            f.write(line+"\n")
+        line  = "{county},{precinctID},\"{mapLabelMatched}\",\"{precinct}\",{score:.2f}".format(county=county,precinctID=precinctID,precinct=precinct,mapLabelMatched=mapLabelMatched[0],score=mapLabelMatched[1])
+        f.write(line+"\n")
         if mapLabelMatched[1] < 95 :
             print(line)
 
