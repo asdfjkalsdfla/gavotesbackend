edda53d926f8ef5bb671f06afd199200769f363d
Tom Thumb <74577069+asdfjkalsdfla@users.noreply.github.com>
2024-05-19 14:33:48 -0400

    add 2024 primary absentee data

--- copyToFrontEnd.sh
+++ copyToFrontEnd.sh
@@ -8,7 +8,7 @@ mkdir -p data/frontend/electionResults
 mkdir -p data/frontend/demographics
 mkdir -p data/frontend/shapeFiles
 
-elections=( 2016_general 2018_general 2020_general 2021_senate_runoff 2022_general 2022_runoff )
+elections=( 2016_general 2018_general 2020_general 2021_senate_runoff 2022_general 2022_runoff 2024_primary )
 electionsLive=( 2022_runoff )
 
 declare -A levels
@@ -22,18 +22,21 @@ do
     for level in "${!levels[@]}"
     do
         cp -v ./data/absenteeSummary/$election/$level.json/part*.txt ./data/frontend/absentee/absenteeSummary-$election-${levels[$level]}.json
-        cp -v ./data/electionResultsSummary/$election/$level.json/part*.txt ./data/frontend/electionResults/electionResultsSummary-$election-${levels[$level]}.json
+        if [ -d ./data/electionResultsSummary/$election ]; then
+            cp -v ./data/electionResultsSummary/$election/$level.json/part*.txt ./data/frontend/electionResults/electionResultsSummary-$election-${levels[$level]}.json
+        fi
+
     done
 done
 
 # cp ./data/electionResults/2022_runoff/2022_runoff_map_ids_manual.csv ./data/frontend/2022_general_map_ids_manual.csv
 # cp ./data/electionResults/2022_runoff/2022_runoff_map_ids.csv ./data/frontend/2022_general_map_ids.csv
 
-cp ./data/geojson/precincts/2022_simple/GA_precincts_* ./data/frontend/shapeFiles/.
-cp ./data/electionResults/2022_runoff/2022_runoff_map_2022_ids_manual.csv ./data/frontend/.
-cp ./data/electionResults/2022_runoff/2022_runoff_map_2022_ids.csv ./data/frontend/.
-cp ./data/geojson/precincts/2020_simple/demographics-state-2020.json ./data/frontend/demographics/.
-cp ./data/geojson/precincts/2020_simple/demographics-county-2020.json ./data/frontend/demographics/.
-cp ./data/geojson/precincts/2020_simple/demographics-precinct-2020.json ./data/frontend/demographics/.
+cp -v ./data/geojson/precincts/2022_simple/GA_precincts_* ./data/frontend/shapeFiles/.
+cp -v ./data/electionResults/2022_runoff/2022_runoff_map_2022_ids_manual.csv ./data/frontend/.
+cp -v ./data/electionResults/2022_runoff/2022_runoff_map_2022_ids.csv ./data/frontend/.
+cp -v ./data/geojson/precincts/2020_simple/demographics-state-2020.json ./data/frontend/demographics/.
+cp -v ./data/geojson/precincts/2020_simple/demographics-county-2020.json ./data/frontend/demographics/.
+cp -v ./data/geojson/precincts/2020_simple/demographics-precinct-2020.json ./data/frontend/demographics/.
 
 exit 0
