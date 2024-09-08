2af0e932afbf021323717c3a27176d806eff9cde
Tom Thumb <74577069+asdfjkalsdfla@users.noreply.github.com>
2024-09-08 16:13:34 -0400

    update to 2024 general

--- copyToFrontEnd.sh
+++ copyToFrontEnd.sh
@@ -2,14 +2,14 @@
 # set -e
 
 
-rm -rf ./data/frontend
+# rm -rf ./data/frontend
 mkdir -p data/frontend/absentee
 mkdir -p data/frontend/electionResults
 mkdir -p data/frontend/demographics
 mkdir -p data/frontend/shapeFiles
 
-elections=( 2016_general 2018_general 2020_general 2021_senate_runoff 2022_general 2022_runoff 2024_primary )
-electionsLive=( 2022_runoff )
+# elections=( 2016_general 2018_general 2020_general 2021_senate_runoff 2022_general 2022_runoff 2024_primary )
+elections=( 2024_general )
 
 declare -A levels
 levels["statewide"]="state"
@@ -32,11 +32,11 @@ done
 # cp ./data/electionResults/2022_runoff/2022_runoff_map_ids_manual.csv ./data/frontend/2022_general_map_ids_manual.csv
 # cp ./data/electionResults/2022_runoff/2022_runoff_map_ids.csv ./data/frontend/2022_general_map_ids.csv
 
-cp -v ./data/geojson/precincts/2022_simple/GA_precincts_* ./data/frontend/shapeFiles/.
-cp -v ./data/electionResults/2022_runoff/2022_runoff_map_2022_ids_manual.csv ./data/frontend/.
-cp -v ./data/electionResults/2022_runoff/2022_runoff_map_2022_ids.csv ./data/frontend/.
-cp -v ./data/geojson/precincts/2020_simple/demographics-state-2020.json ./data/frontend/demographics/.
-cp -v ./data/geojson/precincts/2020_simple/demographics-county-2020.json ./data/frontend/demographics/.
-cp -v ./data/geojson/precincts/2020_simple/demographics-precinct-2020.json ./data/frontend/demographics/.
+# cp -v ./data/geojson/precincts/2022_simple/GA_precincts_* ./data/frontend/shapeFiles/.
+# cp -v ./data/electionResults/2022_runoff/2022_runoff_map_2022_ids_manual.csv ./data/frontend/.
+# cp -v ./data/electionResults/2022_runoff/2022_runoff_map_2022_ids.csv ./data/frontend/.
+# cp -v ./data/geojson/precincts/2020_simple/demographics-state-2020.json ./data/frontend/demographics/.
+# cp -v ./data/geojson/precincts/2020_simple/demographics-county-2020.json ./data/frontend/demographics/.
+# cp -v ./data/geojson/precincts/2020_simple/demographics-precinct-2020.json ./data/frontend/demographics/.
 
 exit 0
