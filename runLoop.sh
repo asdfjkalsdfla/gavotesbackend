2eb00e980fb926a54c8f0745ebf2ee26ca099bc7
Tom Thumb <74577069+asdfjkalsdfla@users.noreply.github.com>
2024-09-08 15:43:34 -0400

    fix uv run

--- runLoop.sh
+++ runLoop.sh
@@ -2,7 +2,7 @@
 set -e
 set -x
 ./electionResultsPull.js -e 116564 -o data/electionResults/2022_runoff/2022_runoff.csv
-uv electionResultPrecinctNamelToID.py 2022_runoff --mapYear 2022 > precinct_changes_2022.csv
-uv main.py
+uv run electionResultPrecinctNamelToID.py 2022_runoff --mapYear 2022 > precinct_changes_2022.csv
+uv run main.py
 ./copyToFrontEnd.sh
 ./deploy.sh
