c215a42c100bb85dabf9467fec458bec84617512
Tom Thumb <74577069+asdfjkalsdfla@users.noreply.github.com>
2024-11-10 12:35:11 -0500

    pull 2024 data

--- runLoop.sh
+++ runLoop.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 set -e
 set -x
-./electionResultsPull.js -e 116564 -o data/electionResults/2022_runoff/2022_runoff.csv
-uv run electionResultPrecinctNamelToID.py 2022_runoff --mapYear 2022 > precinct_changes_2022.csv
+node electionResultsPull2024.js -r "President" -o data/electionResults/2024/2024_general.csv -e "2024NovGen"
+uv run electionResultPrecinctNamelToID.py 2024_general --mapYear 2022 --raceYear 2024 > precinct_changes_2024.csv
 uv run main.py
 ./copyToFrontEnd.sh
 ./deploy.sh
